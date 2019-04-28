065ec7375634af77b3fdc114c77e0d79784d02b2
Thom David <thomdavid74@gmail.com>
2019-04-28 14:56:06 -0700

    Dropdown Class

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ A small UI library using Babel and Webpack boilerplate.
   npm install to install required dependencies  
   npm run serve to spin up a local server on port 8080  
   npm run build to create the deployable build version in the dist directory  
+
+  ## Functionality
+  Tooltip - creates a tooltip Class to create tooltips to add to a UI.  
+  Dropdowns - creates a dropdown Class to add dropdowns to a UI.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import Tooltip from './ui-scripts/tooltip';
+import Dropdown from './ui-scripts/dropdown';
 
-// create tooltip
+// create tooltips manually
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
 const tooltip2 = new Tooltip(document.querySelector('.test-tip'));
-
 
 tooltip.init();
 tooltip2.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
