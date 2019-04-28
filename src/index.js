fe656f0d0be6f1a884fe5182618f55e84ac9e6bd
Thom David <thomdavid74@gmail.com>
2019-04-28 14:01:29 -0700

    Tooltip class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './test.css';
+import Tooltip from './ui-scripts/tooltip';
+
+// create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+const tooltip2 = new Tooltip(document.querySelector('.test-tip'));
+
+
+tooltip.init();
+tooltip2.init();
