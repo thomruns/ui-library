19dc99607405278e3b66df90237477c7bbb14070
Thom David <thomdavid74@gmail.com>
2019-04-29 11:47:57 -0700

    Comment index.js controller

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// import the components
 import Tooltip from './ui-scripts/tooltip';
 import Dropdown from './ui-scripts/dropdown';
 import Tabs from './ui-scripts/tabs';
@@ -11,22 +12,29 @@ tooltip.init();
 tooltip2.init();
 
 // create dropdowns
+//  get all elements with a class of dropdown, returns a node list
 const dropdowns = document.querySelectorAll('.dropdown');
-
+// iterate over the node list
 dropdowns.forEach(dropdown => {
+  // create a new Dropdown instance for each element in the nodelist
   const instance = new Dropdown(dropdown);
+  // call the init function
   instance.init();
 });
 
 // create tabs
+// create new Tabs instance on tabs container
 const tabs = new Tabs(document.querySelector('.tabs'));
+// call the init funcion
 tabs.init();
 
 // create snackbar
 const snackbar = new Snackbar();
 snackbar.init();
 
+// get the DOM button element for the snackbar
 const button = document.querySelector('.snackbar-trigger');
+// add a click event listener
 button.addEventListener('click', () => {
   snackbar.show('You clicked me :)');
 });
